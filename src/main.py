60728e64f2a09258add48eca991aac0863549e50
chiarasia <chiarasia@ukr.net>
2026-07-28 21:51:12 +0300

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,18 @@
+from cli.menu import show_menu
+from cli.commands import execute
+
+def main():
+
+    while True:
+
+        show_menu()
+
+        choice = input("Choose: ")
+
+        if choice == "5":
+            break
+
+        execute(choice)
+
+if __name__ == "__main__":
+    main()
